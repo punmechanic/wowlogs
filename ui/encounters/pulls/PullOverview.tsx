49411dff6f995ebc76081853185ceb67e731733d
punmechanic <2939173+punmechanic@users.noreply.github.com>
2024-11-04 07:37:05 -0800

    Configure the example damage chart some more

--- ui/encounters/pulls/PullOverview.tsx
+++ ui/encounters/pulls/PullOverview.tsx
@@ -1,5 +1,5 @@
 import { Chart, ChartConfiguration, ChartData } from "chart.js/auto";
-import { addSeconds } from "date-fns";
+import { addSeconds, getMilliseconds } from "date-fns";
 import { useEffect, useMemo, useRef } from "react";
 import 'chartjs-adapter-date-fns';
 
@@ -7,38 +7,48 @@ export async function loadData() {
 	return null;
 }
 
+interface Timeseries {
+	name: string
+	// HACK: any is used to prevent TypeScript from complaining about us using Date, which is a valid option for time series axes.
+	points: { x: any, y: number }[]
+}
+
+
+function generateDummySeries(): Timeseries[] {
+	const date = new Date();
+	const offsetSeconds = 5;
+	const sets = ['Warrior', 'Mage'];
+	const numPoints = 100;
+
+	return sets.map(name => {
+		const points = [];
+		for (let i = 0; i < numPoints; i++) {
+			points.push({
+				y: Math.random() * 200,
+				x: addSeconds(date, i * offsetSeconds)
+			});
+		}
+		return {
+			name,
+			points
+		};
+	});
+}
 
 function DamageChart() {
-	const data: ChartData = useMemo(() => {
-		const DATA_COUNT = 12;
-		// const labels = [];
-
-		// This is dummy data.
-		const date = new Date();
-		// for (let i = 0; i < DATA_COUNT; ++i) {
-		// 	labels.push(i.toString());
-		// }
-
-		const offsetSeconds = 5;
-		const data = [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170];
-		const points = data.map((val, index) => {
+	const data = useMemo(() => {
+		const data = generateDummySeries();
+		const datasets = data.map(set => {
 			return {
-				y: val,
-				x: addSeconds(date, index * offsetSeconds)
+				label: set.name,
+				data: set.points,
+				fill: false,
+				cubicInterpolationMode: 'monotone' as const
 			}
 		});
 
-		return {
-			labels: [],
-			datasets: [
-				{
-					label: 'Damage',
-					data: points,
-					fill: false,
-					cubicInterpolationMode: 'monotone',
-				}
-			]
-		};
+		// TODO: Add an All series which is the cumulative sum of all entries.
+		return { datasets }
 	}, []);
 
 	const chartRef = useRef<HTMLCanvasElement>(null);
@@ -66,6 +76,9 @@ function DamageChart() {
 					x: {
 						display: true,
 						type: 'timeseries',
+						time: {
+							unit: 'second'
+						}
 					},
 					y: {
 						display: true,
