d3b7510bca6c71709c395ab55129c65b3e939800
punmechanic <2939173+punmechanic@users.noreply.github.com>
2024-11-03 21:56:32 -0800

    Add better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
+use std::str::FromStr;
+
 use chrono::NaiveDateTime;
+use thiserror::Error;
 
 fn main() {
     println!("Hello, world!");
 }
 
 /// A raw, unprocessed log record.
+#[derive(PartialEq, Debug)]
 struct Record {
     timestamp: NaiveDateTime,
+}
+
+#[derive(Error, Debug, PartialEq)]
+enum RecordParseError {
+    #[error("header is malformed")]
+    HeaderMalformed,
+    #[error("failed to parse timestamp")]
+    InvalidTimestamp {
+        #[from]
+        source: chrono::ParseError,
+    },
 }
 
 impl Record {
@@ -29,14 +44,13 @@ impl Record {
     }
 }
 
-impl TryFrom<&str> for Record {
-    type Error = ();
+impl FromStr for Record {
+    type Err = RecordParseError;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let mut parts = value.split("  ");
-        // TODO: Replace unwrap
-        let header = parts.next().unwrap();
-        let ts = Self::parse_timestamp(header).unwrap();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split_once("  ");
+        let (head, _tail) = parts.ok_or(RecordParseError::HeaderMalformed)?;
+        let ts = Self::parse_timestamp(head)?;
 
         Ok(Record { timestamp: ts })
     }
@@ -46,10 +60,10 @@ impl TryFrom<&str> for Record {
 mod tests {
     use chrono::{NaiveDate, NaiveTime};
 
-    use crate::Record;
+    use crate::{Record, RecordParseError};
 
     #[test]
-    fn can_parse_timestamp_pacific() -> Result<(), chrono::ParseError> {
+    fn parse_timestamp_pacific() -> Result<(), RecordParseError> {
         let raw = "10/2/2024 17:36:02.460-7";
         let ts = Record::parse_timestamp(raw)?;
         let date = NaiveDate::from_ymd_opt(2024, 10, 2).unwrap();
@@ -61,7 +75,7 @@ mod tests {
     }
 
     #[test]
-    fn can_parse_timestamp_bst() -> Result<(), chrono::ParseError> {
+    fn parse_timestamp_bst() -> Result<(), RecordParseError> {
         let raw = "10/2/2024 17:36:02.460+1";
         let ts = Record::parse_timestamp(raw)?;
         let date = NaiveDate::from_ymd_opt(2024, 10, 2).unwrap();
@@ -73,7 +87,7 @@ mod tests {
     }
 
     #[test]
-    fn can_parse_timestamp_utc() -> Result<(), chrono::ParseError> {
+    fn parse_timestamp_utc() -> Result<(), RecordParseError> {
         let raw = "10/2/2024 17:36:02.460+0";
         let ts = Record::parse_timestamp(raw)?;
         let date = NaiveDate::from_ymd_opt(2024, 10, 2).unwrap();
@@ -85,13 +99,30 @@ mod tests {
     }
 
     #[test]
-    fn can_parse_record() {
+    fn parse_error_on_malformed_header() -> Result<(), RecordParseError> {
+        let bad_headers = [
+            // Missing two spaces between timestamp and rest of content
+            "10/2/2024 17:34:00.153-7 COMBAT_LOG_VERSION,21,ADVANCED_LOG_ENABLED,1,BUILD_VERSION,11.0.2,PROJECT_ID,1",
+        ];
+
+        for header in bad_headers {
+            let res: Result<Record, RecordParseError> = header.parse();
+            assert_eq!(res, Err(RecordParseError::HeaderMalformed));
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn parse() -> Result<(), RecordParseError> {
         let entry = "10/2/2024 17:34:00.153-7  COMBAT_LOG_VERSION,21,ADVANCED_LOG_ENABLED,1,BUILD_VERSION,11.0.2,PROJECT_ID,1";
-        let record: Record = entry.try_into().unwrap();
+        let record: Record = entry.parse()?;
 
         let date = NaiveDate::from_ymd_opt(2024, 10, 2).unwrap();
         let time = NaiveTime::from_hms_milli_opt(17, 34, 0, 153).unwrap();
         let dt = date.and_time(time);
         assert_eq!(record.timestamp, dt);
+
+        Ok(())
     }
 }
